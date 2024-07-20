ae61c892c5af89f4a0f96d91772777ed139c31ad
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-07-20 15:23:31 -0700

    linked a test database from MySQL to server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
-import express from "express"
+import express from "express";
 import cors from "cors";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 import createRouter from "express-file-routing";
-import { config } from 'dotenv';
+import { config } from "dotenv";
 
 config();
+const app = express();
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
+const SERVER_SRC = path.join(__dirname, "src");
 
 const PORT = process.env.PORT;
-const SERVER_SRC = path.join(__dirname, 'src');
-const app = express();
+
 const corsOptions = {
-    origin: ["http://localhost:5173"]
+    origin: ["http://localhost:5173"],
 };
 
 app.use(cors(corsOptions));
@@ -22,5 +23,5 @@ app.use(cors(corsOptions));
 await createRouter(app, { directory: path.join(SERVER_SRC, "routes") }); // as wrapper function
 
 app.listen(PORT, () => {
-    console.log(`App listening on port: ${PORT}`)
+    console.log(`App listening on port: ${PORT}`);
 });
