36d3cd8a1fc8899c9bc30f28b0f6dcdc81effde8
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-07-20 12:34:42 -0700

    setup axios for HTTP requests and setup cors

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors";
 import path from 'path';
 import { fileURLToPath } from 'url';
 import createRouter, { router } from "express-file-routing"
@@ -9,6 +10,11 @@ const __dirname = path.dirname(__filename); // get the name of the directory
 const PORT = 3000;
 const SERVER_SRC = path.join(__dirname, 'src');
 const app = express();
+const corsOptions = {
+    origin: ["http://localhost:5173"]
+};
+
+app.use(cors(corsOptions));
 
 await createRouter(app, { directory: path.join(SERVER_SRC, "routes") }); // as wrapper function
 
