5f469482fef9e086e4e1b9fcd50787ad4995d05a
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-07-28 15:55:16 -0700

    added messages to userSchema

--- server/src/config/joi-schemas.js
+++ server/src/config/joi-schemas.js
@@ -1,7 +1,21 @@
-import Joi from "joi";
+import Joi from 'joi';
+
+const USERNAME_MIN_LEN = 8;
+const USERNAME_MAX_LEN = 30;
+const USER_EMAIL_MAX_LEN = 125;
 
 export const userSchema = Joi.object({
-	userName: Joi.string().trim().min(8).max(30).required(),
-	userEmail: Joi.string().email().trim().max(125).required(),
-	password: Joi.string().required().messages(),
+	userName: Joi.string().trim().min(USERNAME_MIN_LEN).max(USERNAME_MAX_LEN).required().messages({
+		'string.base': 'Username must be of type string.',
+        "string.min": `Username must be ${USERNAME_MIN_LEN} characters long`,
+        "string.max": `Username must be no more than ${USERNAME_MAX_LEN} characters long`,
+		'any.required': 'An username is required.',
+	}),
+	userEmail: Joi.string().email().trim().max(USER_EMAIL_MAX_LEN).required().messages({
+		'string.base': 'Email must be of type string.',
+		'string.email': 'The string is not a valid email.',
+        "string.max": `Email must be no more than ${USER_EMAIL_MAX_LEN} characters long`,
+		'any.required': 'An email is required.',
+	}),
+	password: Joi.string().required(),
 });
