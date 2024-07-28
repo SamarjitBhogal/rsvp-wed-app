2b167701088fe2e7d835d9a4ba6e021da5fa013f
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-07-28 16:45:20 -0700

    implmented bcrypt with signup

--- server/src/routes/user/signup.js
+++ server/src/routes/user/signup.js
@@ -1,8 +1,8 @@
 import { User } from '../../models/user.js';
 import { userSchema } from '../../config/joi-schemas.js';
+import { createHashedPassword } from '../../utils/bcrypt.js';
 
 export const post = async (req, res) => {
-	// do Joi input handling and send status responses for client error
 	const result = userSchema.validate(req.body);
 	if (result.error)
 		return res.status(400).send({ message: 'Could not signup.', error: result.error.details[0].message });
@@ -10,9 +10,9 @@ export const post = async (req, res) => {
     let { userName, userEmail, password } = req.body;
 
 	try {
-		let user = new User(userName, userEmail, password);
+		let user = new User(userName, userEmail, createHashedPassword(password));
 		await user.createUser();
-		console.log(result);
+		console.log(user);
 	} catch (error) {
 		// database error
 		return res.status(500).send({ message: 'Failed to create user.', error: error });
--- server/src/utils/bcrypt.js
+++ server/src/utils/bcrypt.js
@@ -3,18 +3,10 @@ import bcrypt from 'bcrypt';
 const SALT_ROUNDS = 10;
 
 export function createHashedPassword(password) {
-	bcrypt.genSalt(SALT_ROUNDS, (err, salt) => {
-		if (err) console.error(err);
-		bcrypt.hash(password, salt, (err, hash) => {
-			if (err) console.error(err);
-			return hash;
-		});
-	});
+	const salt = bcrypt.genSaltSync(SALT_ROUNDS);
+	return bcrypt.hashSync(password, salt);
 }
 
 export function compareHashedPasswords(password, hash) {
-    bcrypt.compare(password, hash, function(err, result) {
-        if (err) console.error(err);
-        return result;
-    });
+	return bcrypt.compareSync(password, hash);
 }
