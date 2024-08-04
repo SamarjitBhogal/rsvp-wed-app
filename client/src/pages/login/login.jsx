189b1e36acfdd98069a0f077888bfce0405e01cd
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-08-03 21:02:51 -0700

    Added and configured tailwind css.

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,11 +1,11 @@
-import styles from "./login.module.css";
+import styles from './login.module.css';
 
 const LoginPage = () => {
-    return (
-        <>
-            Login Page.
-        </>
-    );
-}
+	return (
+		<>
+			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+		</>
+	);
+};
 
 export default LoginPage;
