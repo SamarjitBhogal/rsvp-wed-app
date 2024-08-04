5011755a6737446c3b8798ee40e9ea25cfc4c6fd
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-08-03 17:50:23 -0700

    Linked login page temporarily

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 
+import LoginPage from './pages/login/login';
+
 const App = () => {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			test
+			<LoginPage/>
 		</>
 	);
 }
--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,7 +1,11 @@
-
+import styles from "./login.module.css";
 
 const LoginPage = () => {
-
+    return (
+        <>
+            Login Page.
+        </>
+    );
 }
 
 export default LoginPage;
