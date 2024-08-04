e9db1840e38d2906257573829f9e720c3b291a4a
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-08-04 11:11:27 -0700

    Basic login page completed.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import SignupPage from './pages/signup/signup';
 import LoginPage from './pages/login/login';
 
 const App = () => {
 	const router = createBrowserRouter([
+		{ path: '/', element: <h1>LANDING PAGE</h1> },
+		{ path: '/signup', element: <SignupPage /> },
 		{ path: '/login', element: <LoginPage /> },
 		{ path: '/home', element: <h1>HOME</h1> },
 	]);
--- client/src/pages/signup/signup.jsx
+++ client/src/pages/signup/signup.jsx
@@ -1,20 +1,21 @@
-import styles from './login.module.css';
-import { useNavigate } from "react-router-dom";
+import styles from './signup.module.css';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const LoginPage = () => {
-	const navigate = useNavigate();
+const SignupPage = () => {
+    const navigate = useNavigate();
 
-	const handleLogin = (event) => {
+	const handleSignup = (event) => {
 		event.preventDefault();
 		axios
-			.post('http://localhost:3000/user/login', {
-				userEmail: event.target[0].value,
-				password: event.target[1].value,
+			.post('http://localhost:3000/user/signup', {
+				userName: event.target[0].value,
+				userEmail: event.target[1].value,
+				password: event.target[2].value,
 			})
 			.then((res) => {
 				console.log(res);
-				navigate("/home");
+				navigate('/home');
 			});
 	};
 
@@ -23,18 +24,34 @@ const LoginPage = () => {
 			<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
 				<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
 					{/* <img
-						alt='Your Company'
-						src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
-						className='mx-auto h-10 w-auto'
-					/> */}
+                    alt='Your Company'
+                    src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+                    className='mx-auto h-10 w-auto'
+                    /> */}
 					<p className='text-center'>A LOGO</p>
 					<h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
-						Sign in to your account
+						Create An Account
 					</h2>
 				</div>
 
 				<div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-					<form className='space-y-6' onSubmit={handleLogin}>
+					<form className='space-y-6' onSubmit={handleSignup}>
+						<div>
+							<label htmlFor='username' className='block text-sm font-medium leading-6 text-gray-900'>
+								User Name
+							</label>
+							<div className='mt-2'>
+								<input
+									id='username'
+									name='userName'
+									type='text'
+									required
+									autoComplete='username'
+									className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+								/>
+							</div>
+						</div>
+
 						<div>
 							<label htmlFor='email' className='block text-sm font-medium leading-6 text-gray-900'>
 								Email address
@@ -88,4 +105,4 @@ const LoginPage = () => {
 	);
 };
 
-export default LoginPage;
+export default SignupPage;
