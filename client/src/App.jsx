99258b9b4711bebfde0ae2f4c15201eae6742457
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-08-18 14:31:40 -0700

    Setup basic header from tailwindcss.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import Header from './components/header/header';
+import LandingPage from './pages/landing/landing-page';
 import SignupPage from './pages/signup/signup';
 import LoginPage from './pages/login/login';
 
 const App = () => {
 	const router = createBrowserRouter([
-		{ path: '/', element: <h1>LANDING PAGE</h1> },
+		{ path: '/', element: <LandingPage /> },
 		{ path: '/signup', element: <SignupPage /> },
 		{ path: '/login', element: <LoginPage /> },
 		{ path: '/home', element: <h1>HOME</h1> },
@@ -14,6 +16,7 @@ const App = () => {
 	return (
 		//TODO: Authenticated routes and non-authenticated routes.
 		<>
+			<Header />
 			<RouterProvider router={router} />
 		</>
 	);
---
+++ client/src/pages/landing/landing-page.jsx
@@ -0,0 +1,8 @@
+import styles from './landing-page.module.css';
+import { useNavigate } from 'react-router-dom';
+
+const LandingPage = () => {
+	return <>LandingPage</>;
+};
+
+export default LandingPage;
